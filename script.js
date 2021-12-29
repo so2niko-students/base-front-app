2a5032327105447c7ba500ec28375c07954d7be5
sheniaks <sheniaks@gmail.com>
2021-12-29 19:03:32 +0200

    fixed required blocks

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 //VARIABLES
-const gender = document.getElementById('gender');
-const users = document.getElementById('users');
-const usersLabel = document.getElementById('usersLabel');
-const myBtn = document.getElementById('myBtn');
-const outputBody = document.getElementById('output');
+const gender = document.querySelector('#gender');
+const users = document.querySelector('#users');
+const usersLabel = document.querySelector('#usersLabel');
+const myBtn = document.querySelector('#myBtn');
+const outputBody = document.querySelector('#output');
 let url = `https://randomuser.me/api/`;
 let urlVal = {
-    usersVal : '',
+    usersVal : '?results=1',
     genderVal: ''
 }
 //EVENTS
 myBtn.addEventListener('click', getData);
 users.addEventListener('change', getUsers);
 gender.addEventListener('change', getGender);
+document.querySelector('#sortName').onclick = function(){sortTable(1)};
+document.querySelector('#sortGender').onclick = function(){sortTable(2)};
+document.querySelector('#sortCity').onclick = function(){sortTable(3)};
+document.querySelector('#sortLogin').onclick = function(){sortTable(5)};
+
 
 //USERS FUNCTION
 usersLabel.innerHTML = `1 user`;
 function getUsers() {
-   urlVal.usersVal = `?results=${this.value}`
-   usersLabel.innerHTML = `${users.value} users`
+   urlVal.usersVal = `?results=${this.value}`;
+   usersLabel.innerHTML = `${users.value} users`;
 }
 
 //GENDER FUNCTION
 function getGender() {
-   urlVal.genderVal = `&gender=${this.value}`
+   urlVal.genderVal = `&gender=${this.value}`;
 }
 
 //URL FUNCTION
@@ -38,9 +43,8 @@ async function getData (){
     const resp = await fetch(getUrl());
     const data = await resp.json();
         //console.log(data);
-        let author = data.results;
         let output =``;
-        author.forEach(function(lists) {
+        data.results.map(function(lists) {
             output += `
               <tr>
               <td><img src="${lists.picture.thumbnail}"></td>
@@ -62,8 +66,8 @@ async function getData (){
 
 //SORT FUNCTION
 function sortTable(n) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("myTable");
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.querySelector('#myTable');
     switching = true;
     dir = "asc";
     while (switching) {
@@ -71,8 +75,8 @@ function sortTable(n) {
       rows = table.rows;
       for (i = 1; i < (rows.length - 1); i++) {
         shouldSwitch = false;
-        x = rows[i].getElementsByTagName("TD")[n];
-        y = rows[i + 1].getElementsByTagName("TD")[n];
+        x = rows[i].querySelectorAll('td')[n];
+        y = rows[i + 1].querySelectorAll('td')[n];
         if (dir == "asc") {
           if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
             shouldSwitch = true;
