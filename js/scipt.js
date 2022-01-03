953dab4f1c5959fa66f63db68e6b308f73d0a04d
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-04 01:22:05 +0200

    adding changes after review

--- js/scipt.js
+++ js/scipt.js
@@ -9,49 +9,39 @@ form.addEventListener('submit', () => false);
 form.addEventListener('input', () => output.value = +input.value);
 
 select.addEventListener('click', function () {
-   if (select.value === '') select.classList.remove('select__error');
+   select.classList.remove('select__error');
 })
+
 generateButton.addEventListener('click', function () {
-   if (select.value === '') select.classList.add('select__error');
-   else {
-      let number = document.querySelector('.numout'); //number of users
-      switch (select.value) {
-         case 'All': {
-            fetch(`https://randomuser.me/api/?results=${number.value}`)
-               .then(response => response.json())
-               .then(data => {
-                  createTable(number, data);
-               })
-               .catch(rejected => {
-                  console.log(rejected);
-               });
-            break;
-         }
-         case 'Male': {
-            fetch(`https://randomuser.me/api/?results=${number.value}&gender=male`)
-               .then(response => response.json())
-               .then(data => {
-                  createTable(number, data);
-               })
-               .catch(rejected => {
-                  console.log(rejected);
-               });
-            break;
-         }
-         case 'Female': {
-            fetch(`https://randomuser.me/api/?results=${number.value}&gender=female`)
-               .then(response => response.json())
-               .then(data => {
-                  createTable(number, data);
-               })
-               .catch(rejected => {
-                  console.log(rejected);
-               });
-            break;
-         }
+   const selectGender = select.value;
+   if (selectGender === '') {
+      select.classList.add('select__error');
+      return;
+   }
+
+   let url = `https://randomuser.me/api/?results=${output.value}`;
+
+   switch (selectGender) {
+      case 'Male': {
+         url = `${url}&gender=male`;
+         break;
+      }
+      case 'Female': {
+         url = `${url}&gender=female`;
+         break;
       }
    }
-})
+
+   fetch(url)
+      .then(response => response.json())
+      .then(data => {
+         createTable(output, data);
+      })
+      .catch(rejected => {
+         console.log(rejected);
+      });
+
+});
 
 function createTable(number, data) {
    document.querySelector('.main').innerHTML = ''; //clear main content
@@ -66,9 +56,7 @@ function createTable(number, data) {
    for (let i = 0; i < number.value; i++) {
       let tr = tablebody.insertRow(i); //add rows
       let short = data.results[i];
-      let gender = short.gender;
-      if (gender === 'female') gender = '♀';
-      else gender = '♂';
+      const gender = short.gender === 'female' ? '♀' : '♂';
 
       tr.innerHTML = `<td><img src='${short.picture.thumbnail}'></img>
             </td><td>${short.name.first + ' ' + short.name.last}</td>
@@ -76,70 +64,52 @@ function createTable(number, data) {
             <td>${short.login.username}</td><td>${short.login.password}</td><td>${short.nat}</td>
             <td>${short.dob.age}</td><td>${short.dob.date}</td><td>${short.cell}</td>`;
    }
+
    document.querySelector('main').appendChild(table); //add a table filling
 
-   [...document.querySelectorAll('.sort')].forEach(function (item) {
-      item.addEventListener('click', function () {
-         this.classList.toggle('active');
-         if (this.classList.contains('active')) {
-            switch (true) {
-               case (this.classList.contains('name')): {
-                  let sortedRows = sortRows(1);
-                  table.append(...sortedRows);
-                  break;
-               }
-               case (this.classList.contains('gender') && select.value === 'All'): {
-                  let sortedRows = sortRows(2);
-                  table.append(...sortedRows);
-                  break;
-               }
-               case (this.classList.contains('city')): {
-                  let sortedRows = sortRows(3);
-                  table.append(...sortedRows);
-                  break;
-               }
-               case (this.classList.contains('login')): {
-                  let sortedRows = sortRows(5);
-                  table.append(...sortedRows);
-                  break;
-               }
-            }
-         }
-         else {
-            switch (true) {
-               case (this.classList.contains('name')): {
-                  let reverseSortedRows = reverseSortRows(1);
-                  table.append(...reverseSortedRows);
-                  break;
-               }
-               case (this.classList.contains('gender') && select.value === 'All'): {
-                  let reverseSortedRows = reverseSortRows(2);
-                  table.append(...reverseSortedRows);
-                  break;
-               }
-               case (this.classList.contains('city')): {
-                  let reverseSortedRows = reverseSortRows(3);
-                  table.append(...reverseSortedRows);
-                  break;
-               }
-               case (this.classList.contains('login')): {
-                  let reverseSortedRows = reverseSortRows(5);
-                  table.append(...reverseSortedRows);
-                  break;
-               }
-            }
-         }
+   [...document.querySelectorAll('.sort')].forEach(function (item) { //add sort listeners
+      item.addEventListener('click', tableSortFunc);
+   });
 
-         function reverseSortRows(i) {
-            return Array.from(table.rows).slice(1)
-               .sort((rowA, rowB) => rowA.cells[i].innerHTML < rowB.cells[i].innerHTML ? 1 : -1);
-         }
+   function tableSortFunc() {
+      this.classList.toggle('active');
+      const direction = this.classList.contains('active') ? 1 : -1;
+      let pref = 0;
 
-         function sortRows(i) {
-            return Array.from(table.rows).slice(1)
-               .sort((rowA, rowB) => rowA.cells[i].innerHTML > rowB.cells[i].innerHTML ? 1 : -1);
-         }
-      })
-   })
+      /*    switch (true) {
+            case (this.classList.contains('name')): {
+               pref = 1;
+               break;
+            }
+            case (this.classList.contains('gender')): {
+               pref = 2;
+               break;
+            }
+            case (this.classList.contains('city')): {
+               pref = 3;
+               break;
+            }
+            case (this.classList.contains('login')): {
+               pref = 5;
+               break;
+            }
+         } */
+
+      const classesCount = {
+         name: 1,
+         gender: 2,
+         city: 3,
+         login: 5
+      };
+
+      const className = this.className.replace('sort', '').replace('active', '').trim();
+      pref = classesCount[className]
+      let sortedRows = sortRows(pref, direction);
+      table.append(...sortedRows);
+
+      function sortRows(i, direction = 1) {
+         return Array.from(table.rows).slice(1)
+            .sort((rowA, rowB) => direction * (rowA.cells[i].innerHTML > rowB.cells[i].innerHTML ? 1 : -1));
+      }
+   };
 }
-
